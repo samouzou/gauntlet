e0ea6f48a9e368f06cb5bf61c058d4b6068f9919
Serge Amouzou <serge@tryverza.com>
2026-02-06 05:58:42 +0000

    Try fixing this error: `Runtime Error: Body exceeded 1 MB limit. To conf

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverActions: {
+    bodySizeLimit: '10mb',
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -23,7 +26,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https'
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
