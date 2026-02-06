240fa8aafb7e5e895d16c299aa934e8d232a10e6
Serge Amouzou <serge@tryverza.com>
2026-02-06 05:37:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7012397261-f7ef4",
+  "appId": "1:753850142067:web:94746f3dd98238b42d8906",
+  "apiKey": "AIzaSyAn_GJiU-jDqwosuopq6kAF0k0zcEd5g30",
+  "authDomain": "studio-7012397261-f7ef4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "753850142067"
+};
