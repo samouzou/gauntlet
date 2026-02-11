7027e772beebf7b74aa7608991b307dceed8d787
Serge Amouzou <serge@tryverza.com>
2026-02-11 19:55:56 +0000

    for stripe, let's use the api version 2025-06-30.basil

--- src/app/actions/checkout.ts
+++ src/app/actions/checkout.ts
@@ -3,7 +3,9 @@ import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+    apiVersion: '2025-06-30.basil',
+});
 
 interface CreateCheckoutSessionProps {
     userId: string;
